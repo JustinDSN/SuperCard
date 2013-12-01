df3c044aab17873de64ffb52df40d5adae00ca57
Justin Steffen <JustinDSN@gmail.com>
2013-11-30 19:09:17 -0800

    Initial Commit

---
+++ SuperCard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SuperCard
+//
+//  Created by Justin Steffen on 11/30/13.
+//  Copyright (c) 2013 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SuperCard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by Justin Steffen on 11/30/13.
+//  Copyright (c) 2013 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
